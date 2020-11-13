8ee57bd8cf2001885624ec52cbfdfa223d960e41
Gaurang-1 <72252176+Gaurang-1@users.noreply.github.com>
2020-11-13 21:35:51 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -23,3 +23,30 @@ var firebaseConfig = {
         likes:0
     });
 }
+
+function getData() { firebase.database().ref("/"+room_name).on('value', 
+function(snapshot) { document.getElementById("output").innerHTML = "";  
+snapshot.forEach(function(childSnapshot) { childKey = childSnapshot.key; 
+  childData = childSnapshot.val(); if(childKey != "purpose") { firebase_message_id = childKey; message_data = childData; 
+    //Start code
+    console.log(message_data);
+    var name=message_data['name'];
+    message=message_data['message'];
+    likes=message_data['like'];
+    name_with_tag="<h4>"+name+"<img src='tick.png' style='height:50px;width:50px;'></h4>";
+    message_with_tag="<h4 class='message_h4'>"+message+"</h4>";
+    like_button="<button class='btn btn-warning' id="+firebase_message_id+"onclick='uppdate_like(this.id)'>";
+    span_with_tag="<span class='glyphicon glyphicon-thumbs-up '>Like :" +likes+"</span></button><hr>";
+    
+    row=name_with_tag+message_with_tag+like_button+span_with_tag;
+    document.getElementById("output").innerHTML=row;
+    
+  //End code 
+} }); }); } getData();
+
+function logOut(){
+ 
+    localStorage.removeItem("user_name");
+    localStorage.removeItem("room_name");
+    window.location="index.html";
+}
